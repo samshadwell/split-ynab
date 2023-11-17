c463833eef85f632e5063ef778287fef9b762a65
samshadwell <git@samshadwell.com>
2023-11-16 23:26:59 -0500

    Clean up config error formatting.

--- config.go
+++ config.go
@@ -15,7 +15,7 @@ type Config struct {
 func NewConfig() *Config {
 	f, err := os.Open("config.yml")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error opening config file. Did you create a config.yml? %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error opening config file. Did you create a config.yml?\n\t%v\n", err)
 		os.Exit(1)
 	}
 	defer f.Close()
@@ -24,7 +24,8 @@ func NewConfig() *Config {
 	decoder := yaml.NewDecoder(f)
 	err = decoder.Decode(&cfg)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error decoding config file. Make sure it has the correct format. See config.yml.example for an example %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error decoding config file. Make sure it has the correct format. See config.yml.example for an example\n\t%v\n", err)
+		os.Exit(1)
 	}
 
 	return &cfg
