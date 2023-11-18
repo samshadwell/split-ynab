1793e296ac676caab7387c560fd6822149877407
samshadwell <git@samshadwell.com>
2023-11-18 13:17:14 -0500

    Switch to generated client.
    Switch to a client generated via OpenAPI generator instead of using
    https://github.com/brunomvsouza/ynab.go
    
    This is because I need some fields that client does not return,
    like `last_knowledge_of_server`.

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/samshadwell/split-ynab
 go 1.21
 
 require (
-	github.com/brunomvsouza/ynab.go v1.4.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/oapi-codegen/runtime v1.1.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
 
-	"github.com/brunomvsouza/ynab.go"
+	"github.com/samshadwell/split-ynab/ynab"
 )
+
+const ynabServer = "https://api.ynab.com/v1"
 
 func main() {
 	config, err := LoadConfig()
@@ -13,13 +17,26 @@ func main() {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
-	client := ynab.NewClient(config.YnabToken)
 
-	transactions, err := client.Transaction().GetTransactionsByAccount(config.BudgetId, config.SplitAccountIds[0], nil)
+	client, err := constructClient(config.YnabToken)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while fetching transactions from YNAB:\n\t%v\n", err)
+		fmt.Fprintf(os.Stderr, "Error while constructing client: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("Server knowledge: ", transactions[0])
+	budgetsResponse, err := client.GetBudgetsWithResponse(context.TODO(), &ynab.GetBudgetsParams{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while getting budgets: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Budgets: %v\n", budgetsResponse.JSON200.Data.Budgets)
+}
+
+func constructClient(authToken string) (*ynab.ClientWithResponses, error) {
+	authRequestEditor := func(ctx context.Context, req *http.Request) error {
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", authToken))
+		return nil
+	}
+	return ynab.NewClientWithResponses(ynabServer, ynab.WithRequestEditorFn(authRequestEditor))
 }
