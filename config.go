b6e734a82d0e86669a85050b55e40700c9effc5b
samshadwell <git@samshadwell.com>
2023-11-17 19:02:48 -0500

    Add new config field.
    Adds `SplitAccountIds` field to config. Also changes error handling
    to the more idiomatic err return style.

--- config.go
+++ config.go
@@ -7,26 +7,36 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-type Config struct {
-	YnabToken string `yaml:"ynabToken"`
-	BudgetId  string `yaml:"budgetId"`
+type config struct {
+	YnabToken       string   `yaml:"ynabToken"`
+	BudgetId        string   `yaml:"budgetId"`
+	SplitAccountIds []string `yaml:"splitAccountIds"`
 }
 
-func NewConfig() *Config {
+func LoadConfig() (*config, error) {
 	f, err := os.Open("config.yml")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error opening config file. Did you create a config.yml?\n\t%v\n", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("Error opening config file. Did you create a config.yml?\n\t%w\n", err)
 	}
 	defer f.Close()
 
-	var cfg Config
 	decoder := yaml.NewDecoder(f)
+	var cfg config
 	err = decoder.Decode(&cfg)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error decoding config file. Make sure it has the correct format. See config.yml.example for an example\n\t%v\n", err)
-		os.Exit(1)
+		return nil, fmt.Errorf(
+			"Error decoding config file. Make sure it has the correct format. See config.yml.example for an example\n\t%w\n",
+			err,
+		)
 	}
 
-	return &cfg
+	if len(cfg.YnabToken) == 0 {
+		return nil, fmt.Errorf("Error: config file is missing ynabToken")
+	}
+
+	if len(cfg.BudgetId) == 0 {
+		return nil, fmt.Errorf("Error: config file is missing budgetId")
+	}
+
+	return &cfg, nil
 }
